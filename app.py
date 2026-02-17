fac4e06332dc9593553ece7719a65e4fb5a7d525
IDGS-803-23001511 <arellanoestefania108@gmail.com>
2026-02-16 21:02:50 -0600

    16/02/2026 Creacion de la tabla

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+from flask import g 
 
+
+import forms
+from model import db
+from model import Alumnos
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html")
+
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("index.html")
-@app.route ("/alumnos")
+
+@app.route("/alumnos")
 def alumnos():
-	return render_template("index.html")
+	return render_template("Alumnos.html")
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	app.run()
+	
--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from re import DEBUG
 from sqlalchemy import create_engine
 
-class Config(object)
-    SECRET_KEY=0"ClaveSecreta"
-    SESSION_COOKIE_SECURE=False
+class Config(object):
+    SECRET_KEY = "ClaveSecreta"
+    SESSION_COOKIE_SECURE = False
 
-class DevelopmentConfig (Config)
-    DEBUG=True
-    SQALCHEMY_DATABASE_URI='mysql+pymsql://root:isabb150108@127.0.0.1/bdidgs803'
-    SQALCHEMY_TRACK_MODIFICATIONS=False    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:isabb150108@127.0.0.1/bdidgs803'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

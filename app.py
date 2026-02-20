609a1e6048e6dfa526bf5047a47b623e2dfdfd94
IDGS-803-23001511 <arellanoestefania108@gmail.com>
2026-02-19 21:14:32 -0600

    19/02/2026 Detalles

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
-from flask import Flask, render_template,request,redirect,url_for
-from flask import flash
-from flask_wtf.csrf import CSRFProtect
-from config import DevelopmentConfig
-from flask import g 
+from flask import Flask, render_template
 
-
-import forms
-from model import db
-from model import Alumnos
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
-csrf=CSRFProtect()
+csrf = CSRFProtect()
 
 @app.errorhandler(404)
-def page_not_found(e):
-	return render_template("404.html")
+def page_noy_found(e):
+    return render_template("404.html")
 
-
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	create_form = forms.UserForm(request.form)
+	alumnos = Alumnos.query.all()
+	return render_template("index.html",form=create_form, alumnos=alumnos)
 
-@app.route("/alumnos")
+@app.route("/Alumnos",methods=['POST','GET'])
 def alumnos():
-	return render_template("Alumnos.html")
+	create_form = forms.UserForm(request.form)
+	if request.method == 'POST':
+		alumno = Alumnos(
+			nombre=create_form.nombre.data,
+			aPaterno=create_form.apaterno.data,
+			email=create_form.email.data,
+		)
+		db.session.add(alumno)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("alumnos.html", form=create_form)
+
+@app.route("/detalles",methods=['POST','GET'])
+def detalles():
+	create_form = forms.UserForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		alumn = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+		nombre = alumn.nombre
+		apaterno = alumn.aPaterno
+		correo = alumn.email
+	return render_template(
+     "detalles.html",
+     	alumn= alumn,
+		nombre= nombre,
+		apaterno= apaterno,
+		correo= correo,
+	)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
 	db.init_app(app)
-	with app.app_context():
+	with app.app_context():	
 		db.create_all()
-	app.run()
-	
+	app.run(debug=True)
